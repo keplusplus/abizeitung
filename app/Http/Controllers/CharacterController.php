63470935c5a55e2e11b9341fff5efc82096c5806
keplusplus <34310827+keplusplus@users.noreply.github.com>
2019-09-17 16:08:31 +0200

    Continued so called "steckbrief" function

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -3,14 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Characteristics;
 
 class CharacterController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     public function create() {
         return view('characteristics/create');
     }
 
     public function store()  {
-        dd(request());
+        if(auth()->user()->has_filled) return redirect()->route('home');
+
+        auth()->user()->has_filled = true;
+        auth()->user()->save();
+
+        $data = request()->validate([
+            'birthdate' => 'required'
+        ]);
+
+        $cs = new Characteristics;
+        $cs->member_id = auth()->user()->member->id;
+        $cs->birthdate = $data['birthdate'];
+        $cs->save();
+
+        return redirect()->route('home');
     }
 }
--- resources/views/characteristics/create.blade.php
+++ resources/views/characteristics/create.blade.php
@@ -5,49 +5,28 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Kommentar hinterlassen</div>
+                <div class="card-header">Steckbrief ausfüllen</div>
 
                 <div class="card-body">
-                  @if (isset($success))
-                    <div class="alert alert-success">
-                      Kommentar wurde erfolgreich hinzugefügt!
-                    </div>
-                  @endif
-                  Der Kommentar wird anonym im Steckbrief der gewählten Schülerin / des gewählten Schülers zu sehen sein. Es genügt auch ein einziges Wort, dass die Person beschreibt. Am Ende wird aus allen Einsendungen für jeden Steckbrief eine Liste angefertigt.
-                  <form class="mt-3" method="post" action="{{ url('/comment') }}">
+                    <form class="form" action="{{ url('/charasteristics') }}" method="post">
                     @csrf
-                    <div class="form-group">
-                      <label for="memberInput">Schüler</label>
-                      <select class="form-control" id="memberInput" name="member_id" required>
-                        <option value="" disabled selected>Schüler wählen</option>
-                        @foreach ($members as $member)
-                          <option value="{{ $member->id }}">
-                            {{ $member->firstname . ' ' . $member->lastname }}
-                          </option>
-                        @endforeach
-                      </select>
-                      @if ($errors->has('member_id'))
-                          <span class="invalid-feedback d-block" role="alert">
-                              <strong>{{ $errors->first('member_id') }}</strong>
-                          </span>
-                      @endif
-                    </div>
-                    <div class="form-group">
-                      <label for="commentText">Kommentar</label>
-                      <input type="text" class="form-control" id="commentText" name="comment" maxlength="80" placeholder="Maximal 80 Zeichen" required>
-                      @if ($errors->has('comment'))
-                          <span class="invalid-feedback d-block" role="alert">
-                              <strong>{{ $errors->first('comment') }}</strong>
-                          </span>
-                      @endif
-                    </div>
-                    <div class="form-group">
-                      <button type="submit" class="btn btn-primary">Absenden</button>
-                    </div>
-                  </form>
-                  <div class="text-right">
-                    <a href="{{ url('/') }}">Zurück</a>
-                  </div>
+                        <div class="form-group">
+                            <label for="name">Vor- und Nachnname</label>
+                            <input class="form-control" id="name" type="text" value="{{ Auth::user()->member->firstname . ' ' . Auth::user()->member->lastname }}" disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="bd">Geburtsdatum</label>
+                            <input class="form-control" id="bd" type="date" name="birthdate" value="2002-01-01" >
+                        </div>
+                        @if ($errors->has('bd'))
+                            <span class="invalid-feedback d-block" role="alert">
+                                <strong>{{ $errors->first('bd') }}</strong>
+                            </span>
+                        @endif
+                        <div class="form-group">
+                            <input class="btn btn-primary" type="submit" value="Senden">
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
