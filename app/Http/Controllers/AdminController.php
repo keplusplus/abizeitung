9418555dd2cfcffbe79eec2315094ef84d939f6d
keplusplus <34310827+keplusplus@users.noreply.github.com>
2019-12-12 23:00:06 +0100

    Admin panel tweaks

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Member;
 use App\User;
+use App\Ranking;
 
 class AdminController extends Controller
 {
@@ -26,13 +27,40 @@ class AdminController extends Controller
     }
 
     public function index() {
-
       $m = Member::all();
       $m1 = Member::all()->take(1);
       $uidm1 = $m1[0]->user_id;
 
+      $rankings = Ranking::all();
+
       if(isset($uidm1)) {
-        return view('admin/index', ['members' => $m]);
+        $voted = 0;
+        $filled = 0;
+        foreach ($m as $member) {
+          if($member->user->has_filled) $filled++;
+          if($member->user->has_voted) $voted++;
+        }
+        return view('admin/index', ['members' => $m, 'rankings' => $rankings, 'count_has_voted' => $voted, 'count_has_filled' => $filled]);
+      } else {
+        return view('admin/index', ['generate' => 1, 'members' => $m]);
+      }
+    }
+
+    public function edit_ranking($ranking_id) {
+      $m = Member::all();
+      $m1 = Member::all()->take(1);
+      $uidm1 = $m1[0]->user_id;
+
+      $rankings = Ranking::all();
+
+      if(isset($uidm1)) {
+        $voted = 0;
+        $filled = 0;
+        foreach ($m as $member) {
+          if($member->user->has_filled) $filled++;
+          if($member->user->has_voted) $voted++;
+        }
+        return view('admin/index', ['members' => $m, 'rankings' => $rankings, 'count_has_voted' => $voted, 'count_has_filled' => $filled, 'edit_ranking' => $ranking_id]);
       } else {
         return view('admin/index', ['generate' => 1, 'members' => $m]);
       }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Admin-Panel</div>
 
@@ -16,11 +16,72 @@
                   @else
                     <button type="button" class="btn btn-secondary" disabled>Accounts bereits generiert</button>
                     <hr>
+                    <h2 class="h4">Rankings</h2>
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th scope="col">Typ</th>
+                          <th scope="col">Bezeichnung</th>
+                          <th scope="col">Bezeichnung (alternativ)</th>
+                          <th scope="col">Aktionen</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @foreach($rankings as $r)
+                          @if(isset($edit_ranking) and $edit_ranking == $r->id)
+                            <tr>
+                              <td></td>
+                              <td>{{ $r->title }}</td>
+                              <td>{{ $r->title_woman }}</td>
+                              <td>
+                                <button class="btn btn-success">Speichern</button>
+                              </td>
+                            </tr>
+                          @else
+                            <tr>
+                              @if(!$r->for_teachers)
+                                @if(!$r->pair)
+                                  @if(!$r->both_genders)
+                                    <td>S E</td>
+                                  @else
+                                    <td>S Z</td>
+                                  @endif
+                                @else
+                                  <td>S P</td>
+                                @endif
+                              @else
+                                @if(!$r->pair)
+                                  @if(!$r->both_genders)
+                                    <td>L E</td>
+                                  @else
+                                    <td>L Z</td>
+                                  @endif
+                                @else
+                                  <td>L P</td>
+                                @endif
+                              @endif
+                              <td>{{ $r->title }}</td>
+                              <td>{{ $r->title_woman }}</td>
+                              <td>
+                                <button class="btn btn-primary disabled" disabled>Ändern</button>
+                                <button class="btn btn-primary disabled" disabled>Löschen</button>
+                              </td>
+                            </tr>
+                          @endif
+                        @endforeach
+                      </tbody>
+                    </table>
+                    <small>S = Schüler; L = Lehrer; Z = Zusammen; E = Einzeln; P = Paar.</small>
+                    <hr>
+                    <h2 class="h4">Accountübersicht</h2>
+                    <p><span class="font-weight-bold">{{ $count_has_voted }}</span> Schüler {{ ($count_has_voted != 1 ? "haben" : "hat") }} abgestimmt. <span class="font-weight-bold">{{ $count_has_filled }}</span> Schüler {{ ($count_has_filled != 1 ? "haben" : "hat") }} den Steckbrief ausgefüllt. (Gesamt: {{  $members->count() }})</p>
                     <table class="table">
                       <thead>
                         <tr>
                           <th scope="col">Schüler</th>
                           <th scope="col">Authentifizierungsschlüssel</th>
+                          <th scope="col">Rankings</th>
+                          <th scope="col">Steckbrief</th>
                         </tr>
                       </thead>
                       <tbody>
@@ -28,6 +89,8 @@
                           <tr>
                             <td>{{ $m->firstname . ' ' . $m->lastname }}</td>
                             <td>{{ $m->user->name }}</td>
+                            <td><span class="{{ ($m->user->has_voted ? 'text-success font-weight-bold' : 'font-italic text-muted') }}">{{ ($m->user->has_voted ? 'abgestimmt' : 'ausstehend') }}</span></td>
+                            <td><span class="{{ ($m->user->has_filled ? 'text-success font-weight-bold' : 'font-italic text-muted') }}">{{ ($m->user->has_filled ? 'ausgefüllt' : 'ausstehend') }}</span></td>
                           </tr>
                         @endforeach
                       </tbody>
